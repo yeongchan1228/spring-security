6381c2204275d85ab373ece2e51b78fd05117d28
최영찬 <gks54410@gmail.com>
2022-03-24 21:53:27 +0900

    Feat: 스프링 데이터 JPA 유저 리포지토리 추가

---
+++ src/main/java/springStudy/springSecurity/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package springStudy.springSecurity.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import springStudy.springSecurity.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}

508e11dcc775d5eee0ea3883cad04d0507bf0420
최영찬 <gks54410@gmail.com>
2022-03-25 16:22:44 +0900

    Feat: BCryptPasswordEncoder IOC 등록 추가

--- src/main/java/springStudy/springSecurity/SpringSecurityApplication.java
+++ src/main/java/springStudy/springSecurity/SpringSecurityApplication.java
@@ -2,7 +2,9 @@ package springStudy.springSecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -12,4 +14,8 @@ public class SpringSecurityApplication {
 		SpringApplication.run(SpringSecurityApplication.class, args);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder createBPE(){
+		return new BCryptPasswordEncoder();
+	}
 }

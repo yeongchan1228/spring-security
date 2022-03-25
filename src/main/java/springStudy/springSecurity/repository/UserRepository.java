1fdd3cfbc0aab8d1c9106b446a5ac5c4b2a13f5f
최영찬 <gks54410@gmail.com>
2022-03-25 12:30:16 +0900

    Feat: 유저 이름으로 유저 찾기 기능 추가

--- src/main/java/springStudy/springSecurity/repository/UserRepository.java
+++ src/main/java/springStudy/springSecurity/repository/UserRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import springStudy.springSecurity.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    public User findByUsername(String username);
 }

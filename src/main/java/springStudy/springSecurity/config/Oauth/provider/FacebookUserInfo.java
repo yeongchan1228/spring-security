9b42cecf3cefbff599ffcba3be717fefe9501bff
최영찬 <gks54410@gmail.com>
2022-03-25 18:45:14 +0900

    Feat: Attributes에서 필요한 정보 가져오기

---
+++ src/main/java/springStudy/springSecurity/config/Oauth/provider/FacebookUserInfo.java
@@ -0,0 +1,32 @@
+package springStudy.springSecurity.config.Oauth.provider;
+
+import java.util.Map;
+
+public class FacebookUserInfo implements OAuth2UserInfo{
+
+    private Map<String, Object> attributes; // oauth2User.getAttributes() 받기
+
+    public FacebookUserInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String getProviderId() {
+        return attributes.get("id").toString();
+    }
+
+    @Override
+    public String getProvider() {
+        return "facebook";
+    }
+
+    @Override
+    public String getEmail() {
+        return attributes.get("email").toString();
+    }
+
+    @Override
+    public String getName() {
+        return getProvider() + "_" + getProviderId();
+    }
+}

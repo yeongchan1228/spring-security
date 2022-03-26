f141aebac9adda7e435d689eb327dab0fe2dd87f
최영찬 <gks54410@gmail.com>
2022-03-26 14:31:33 +0900

    Feat: 스프링 컨테이너에 BCryptPasswordEncoder 추가

--- src/main/java/springStudy/jwt/JwtApplication.java
+++ src/main/java/springStudy/jwt/JwtApplication.java
@@ -2,6 +2,8 @@ package springStudy.jwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class JwtApplication {
@@ -10,4 +12,8 @@ public class JwtApplication {
 		SpringApplication.run(JwtApplication.class, args);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder encoder(){
+		return new BCryptPasswordEncoder();
+	}
 }

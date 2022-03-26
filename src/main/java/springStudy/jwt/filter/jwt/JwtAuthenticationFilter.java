391cb11efeb50ce79ff5339ea18b744adb96ff7f
최영찬 <gks54410@gmail.com>
2022-03-26 15:39:16 +0900

    Feat: 권한 처리를 위해 로그인 인증 후 세션 만들기 추가

--- src/main/java/springStudy/jwt/filter/jwt/JwtAuthenticationFilter.java
+++ src/main/java/springStudy/jwt/filter/jwt/JwtAuthenticationFilter.java
@@ -1,13 +1,20 @@
 package springStudy.jwt.filter.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import springStudy.jwt.auth.PrincipalDetails;
+import springStudy.jwt.entity.User;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * 스프링 시큐리티의 UsernamePasswordAuthenticationFilter
@@ -30,13 +37,49 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         System.out.println("로그인 시도 중 ");
 
         // 1번 username, password를 받아서
-
         // 2번 정상인지 로그인 시도
         // authenticationManager로 로그인 시도 시 -> PrincipleDetailsService가 실행된다. -> loadUserByUsername()이 실행됨
-
         // 3번 PrincipleDeatils를 세션에 담는다 -> 권한 관리를 위해서
-
         // 4번 JWT 토큰을 만들어 응답한다.
-        return super.attemptAuthentication(request, response);
+
+        try {
+//            BufferedReader br = request.getReader();
+//            String input = br.readLine();
+//            System.out.println(input);
+            ObjectMapper om = new ObjectMapper(); // Json으로 보낸 데이터만 받는다
+            User user = om.readValue(request.getInputStream(), User.class); // JSon 받는 방식
+            System.out.println("user = " + user);
+
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                    user.getUsername(), user.getPassword()
+            ); // 토큰 생성
+
+            // PrincipalDetailsService의 loadUserByUsername() 실행됨
+            // 인증을 함
+            // 이때 authentication에 로그인 정보가 담긴다.
+            Authentication authentication = authenticationManager.authenticate(authenticationToken);
+            PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+            System.out.println("principalDetails.getUser() = " + principalDetails.getUser());
+
+            // 정상적으로 완료되면 authentication 객체가 session 영역에 저장된다.
+            // 굳이 authentication을 리턴하는 이유는 권한 관리를 시큐리티가 해주기 때문
+            // 굳이 JWT 토큰을 이용하면서 세션을 만들 필요는 없는데 권한 관리 때문에 세션을 만들어 준다.
+            return authentication;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("======================");
+        return null; // 오류 시
+    }
+
+    /**
+     * attemptAuthentication에서 인증이 정상적으로 완료되면 실행됨
+     * jwt 토큰을 만들어서 request 응답한 사용자에게 jwt 토큰 응답
+     */
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        super.successfulAuthentication(request, response, chain, authResult);
     }
 }

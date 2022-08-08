d2b41969ebfd698b67e83cd26dd9c4f7c1c335fa
최영찬 <gks54410@gmail.com>
2022-08-08 15:15:47 +0900

    Initial Commit

---
+++ oauth2-study/src/main/java/oauth2/oauth2study/Oauth2StudyApplication.java
@@ -0,0 +1,13 @@
+package oauth2.oauth2study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2StudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2StudyApplication.class, args);
+	}
+
+}
---
+++ oauth2-study/src/test/java/oauth2/oauth2study/Oauth2StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package oauth2.oauth2study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
